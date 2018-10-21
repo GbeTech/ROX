5bc3728f97fdc6dd67cc5dc765ccb4983d50e154
GbeTech <gbetech@gmail.com>
2018-10-21 04:24:09 +0300

    layla tov

--- util.py
+++ util.py
@@ -2,10 +2,6 @@ import random
 import time
 
 
-def generate_id(size=1000000000):
-    return random.randint(0, size)
-
-
 def random_str():
     import string
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
