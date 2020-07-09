1a2a1d111d302fd28097e27e37dee18457447ce0
Gilad Barnea <giladbrn@gmail.com>
2020-07-09 16:22:45 +0300

    fixed tests that werent working for some reason. maybe py 3.7 vs 3.8

--- main.py
+++ main.py
@@ -2,14 +2,9 @@ from util import get_now
 from bintrees import AVLTree
 import logging
 
+# This is to prevent the notifications from spamming the console while testing.
+# If you still want to see the notifications, set "ALLOW_SUBSCRIBERS_NOTIFICATION" to True.
 ALLOW_SUBSCRIBERS_NOTIFICATION = False
-warning = '\n'.join([
-    'WARNING:',
-    'main.py(5): "ALLOW_SUBSCRIBERS_NOTIFICATION" is set to False.',
-    'This is to prevent the notifications from spamming the console while testing.',
-    'If you still want to see the notifications, set "ALLOW_SUBSCRIBERS_NOTIFICATION" to True.\n',
-    ])
-print(warning)
 
 
 class Logger:
@@ -19,19 +14,19 @@ class Logger:
                             filename=filename,
                             level=logging.INFO,
                             filemode='w')
-
+    
     def log_bid(self, bid, removed=False):
         if removed:
             logging.info(f'BID (rm) | {bid}')
         else:
             logging.info(f'BID | {bid}')
-
+    
     def log_ask(self, ask, removed=False):
         if removed:
             logging.info(f'ASK (rm) | {ask}')
         else:
             logging.info(f'ASK | {ask}')
-
+    
     def log_trade(self, trade):
         logging.info(f'TRADE | {trade}')
         if trade.ask.is_exhausted():
@@ -48,39 +43,39 @@ class Order:
     # order sides
     BID = "b"
     ASK = "a"
-
+    
     def __gt__(self, other):
         return self._key() > other._key()
-
+    
     def __lt__(self, other):
         return self._key() < other._key()
-
+    
     def __ge__(self, other):
         return self._key() >= other._key()
-
+    
     def __le__(self, other):
         return self._key() <= other._key()
-
+    
     def __eq__(self, other):
         return self._key() == other._key()
-
+    
     def __init__(self, side, price, size):
         self.timestamp = get_now()  # since epoch
         if side != self.BID and side != self.ASK:
             msg = '\n'.join([f'Tried to initialize Order instance with illegal order side arg: "{side}".',
                              f'Only "{self.BID}" or "{self.ASK}" allowed.'])
             raise ValueError(msg)
-
+        
         self.side = side  # "b" (bid) or "a" (ask)
         self._size = size
         self.price = price
         self.id = id(self)
         self.sender_id = ''
-
+    
     @property
     def size(self):
         return self._size
-
+    
     @size.setter
     def size(self, value):
         """Normalize Order.size property to None whenever size has been "exhausted".
@@ -89,16 +84,16 @@ class Order:
             self._size = None
         else:
             self._size = value
-
+    
     def _key(self):
         """Orders are compared firstly by price. In case of equal prices, sizes are compared.
         Two equal Orders must have the same price and size."""
         return self.price, self.size
-
+    
     def is_exhausted(self):
         """An exhausted order is one that met all its requirements until 'size' is None."""
         return not self.size
-
+    
     def __repr__(self):
         return f'timestamp: {self.timestamp}, side: {self.side}, price: {self.price}, size: {self.size}, id: {self.id}, sender_id: "{self.sender_id}"'
 
@@ -110,18 +105,18 @@ class Trade:
         self.price = bid.price
         self.buyer_id = bid.sender_id
         self.seller_id = ask.sender_id
-
+        
         # The actual bid and ask objects are used when
         # logging a trade (order.id and order.size are needed)
         # and when notifying subscribers
         self.bid = bid
         self.ask = ask
-
+    
     def _key(self):
         """Trades are compared firstly by price. In case of equal prices, sizes are compared.
         Two equal Trades must have the same price and size."""
         return self.price, self.size
-
+    
     @staticmethod
     def _finalize(bid: Order, ask: Order):
         """Finalizes the trade between passed bid and ask.
@@ -133,59 +128,61 @@ class Trade:
         trade_size = smallest_size
         bid.size -= smallest_size
         ask.size -= smallest_size
-
+        
         return trade_size
-
+    
     def total_value(self):
         return self.price * self.size
-
+    
     def bid_ask_difference(self):
         return self.price - self.ask.price
-
+    
     def __repr__(self):
         return f'timestamp: {self.timestamp}, price: {self.price}, size: {self.size}, buyer_id: "{self.buyer_id}", seller_id: "{self.seller_id}"'
 
 
 class Subscriber:
-
+    
     def __init__(self, sender_id):
         self.id = sender_id
         self.email = f"{self.id}@example.com"
-
+        
         # Values are not removed or changed in self.orders_ids, regardless or the state of the order.
         self.orders_ids = []
-
+    
     def is_subscribed_to_any(self, *orders):
         """Returns True if subscribed to any of the passed orders"""
         return any(order.id in self.orders_ids for order in orders)
-
+    
     def notify(self, trade):
         """Notifies the subscriber of a trade event relating to one her of orders.
         Sends an email to her address, specifying the trade's details, total expense/income, and the status of the order."""
         import time
-
+        
         if trade.buyer_id == self.id:
             # Subscriber is on the buyer's side
             msg = self._generate_bidder_msg(trade)
         else:
             # Subscriber is on the sellers's side
             msg = self._generate_asker_msg(trade)
-
+        
         _connect = lambda port: print(f'\n***Server connected to port: {port}')
-
+        
         _sendmail = lambda address, text: (print(f'\nSending email to {address}:\n\t{text}')
                                            or time.sleep(1)
                                            or print('\nEmail sent successfully'))
         _quit = lambda: print('\nConnection closed***')
-
-        mock_server = {'connect':  _connect,
-                       'sendmail': _sendmail,
-                       'quit':     _quit, }
-
+        
+        mock_server = {
+            'connect':  _connect,
+            'sendmail': _sendmail,
+            'quit':     _quit,
+            }
+        
         mock_server['connect'](25)
         mock_server['sendmail'](self.email, msg)
         mock_server['quit']()
-
+    
     def _generate_asker_msg(self, trade):
         trade_total_value = trade.total_value()
         bid_ask_difference = trade.bid_ask_difference()
@@ -193,11 +190,11 @@ class Subscriber:
             A trade has been completed with your ask order, id: {trade.ask.id}.
             {trade.size} units have been sold at {trade.price}$ each. 
             Total income: {trade_total_value}$.'''
-
+        
         if bid_ask_difference:
             msg += f'''
             Those are {bid_ask_difference} additional dollars per unit, compared to your original sell offer (at {trade.ask.price}$),
-            which translate to {bid_ask_difference*trade.size}$ above what you have originally planned.
+            which translate to {bid_ask_difference * trade.size}$ above what you have originally planned.
             Go buy yourself some ice cream. You deserve it.
             '''
         if trade.ask.is_exhausted():
@@ -205,7 +202,7 @@ class Subscriber:
         else:
             msg += f'Your ask was not exhausted: {trade.ask.size} units left to sell.'
         return msg
-
+    
     def _generate_bidder_msg(self, trade):
         msg = f'''
             A trade has been completed with your bid order, id: {trade.bid.id}.
@@ -215,35 +212,35 @@ class Subscriber:
         if trade.bid.is_exhausted():
             msg += 'Your bid requirements were fully satisfied.'
         else:
-            msg += f'Your bid was not exhausted: {trade.bid.size } units left to buy.'
+            msg += f'Your bid was not exhausted: {trade.bid.size} units left to buy.'
         return msg
 
 
 class OrderBook:
-
+    
     def __init__(self, logfile_full_path='logs/orderbook.log'):
         self.bids = AVLTree()
         self.asks = AVLTree()
-
+        
         # This is to retrieve an order *by order_id* from the trees in O(log(n)). (self.remove_order())
         # self.bids and self.asks are indexed by Order._key(), which is (self.price, self.size).
         self.order_id_key_translate = {}
         self.trades = {}
         self.subscribers = {}
         self.logger = Logger(logfile_full_path)
-
+    
     # O(log(n))
     def show_top(self):
         """Returns the highest bid and lowest ask orders."""
         _, highest_bid = self.bids.max_item()
         _, lowest_ask = self.asks.min_item()
         return highest_bid, lowest_ask
-
+    
     # O(n)
     def show_trades(self):
         """Returns the list of trades sorted by time"""
         return sorted(self.trades.values(), key=lambda trade: trade.timestamp)
-
+    
     # O(n)
     def show_orderbook(self):
         """Prints the current state orderbook in a human readable format"""
@@ -253,7 +250,7 @@ class OrderBook:
         else:
             for i, bid in enumerate(self.bids.values()):
                 msg += f'\n({i}) {bid}'
-
+        
         msg += "\nAsks:"
         if self.asks.is_empty():
             msg += '\n-- No asks --'
@@ -262,13 +259,13 @@ class OrderBook:
                 msg += f'\n({i}) {ask}'
         print(msg)
         return msg
-
+    
     def notify_trade(self, trade, subscribers):
         """Notifies each of the passed subscribers of the trade event."""
         if ALLOW_SUBSCRIBERS_NOTIFICATION:
             for subscriber in subscribers:
                 subscriber.notify(trade)
-
+    
     # O(log(n))
     def add_order(self, order: Order, sender_id):
         """
@@ -283,10 +280,10 @@ class OrderBook:
         they are deleted from their respective trees.
         """
         order.sender_id = sender_id
-
+        
         # used in remove_order to keep O(log(n)) when retrieving order by order_id
         self.order_id_key_translate[order.id] = order._key()
-
+        
         # Add order to subscriber's orders. Create new subscriber if none was found
         subscriber = self.subscribers.get(sender_id)
         if subscriber:
@@ -295,20 +292,20 @@ class OrderBook:
             subscriber = Subscriber(sender_id)
             subscriber.orders_ids.append(order.id)
             self.subscribers[sender_id] = subscriber
-
+        
         if order.side == Order.BID:
             self._add_bid(order)
-
+        
         else:
             self._add_ask(order)
-
+    
     # O(log(n))
     def _add_ask(self, ask: Order):
         self.logger.log_ask(ask)
-
+        
         ask_key = ask._key()
         self.asks.insert(ask_key, ask)
-
+        
         should_continue = True
         while should_continue:
             # Keep trying to finalize trades with current order (ask)
@@ -324,7 +321,7 @@ class OrderBook:
                     should_continue = False
             else:
                 should_continue = False
-
+    
     # O(log(n))
     def _add_bid(self, bid: Order):
         self.logger.log_bid(bid)
@@ -345,7 +342,7 @@ class OrderBook:
                     should_continue = False
             else:
                 should_continue = False
-
+    
     # O(log(n))
     def remove_order(self, order_id, sender_id):
         """
@@ -358,18 +355,18 @@ class OrderBook:
             if bid_to_remove.sender_id == sender_id:
                 self.logger.log_bid(bid_to_remove, removed=True)
                 self.bids.remove(order_key)
-
+        
         else:  # order is not a bid
             ask_to_remove = self.asks.get(order_key)
             if not ask_to_remove:
                 msg = '\n'.join(['Tried to remove order but no such order exists.',
                                  f'Order id: {order_id}. Order key: {order_key}'])
                 raise KeyError(msg)
-
+            
             if ask_to_remove.sender_id == sender_id:
                 self.logger.log_ask(ask_to_remove, removed=True)
                 self.asks.remove(order_key)
-
+    
     # O(log(n))
     def _try_buy(self, bid: Order) -> Trade or None:
         """
@@ -380,27 +377,27 @@ class OrderBook:
         """
         if self.asks.is_empty():
             return None
-
+        
         ask_key, lowest_ask = self.asks.min_item()
-
+        
         trade = None
         if lowest_ask <= bid:  # someone offered a low-enough sell price and a trade will be made
             trade = Trade(bid, lowest_ask)
-
+            
             # log trade
             self.logger.log_trade(trade)
-
+            
             # get all who subscribed to either side
             trade_subscribers = self._subscribers_of_orders(bid, lowest_ask)
-
+            
             # notify them about the trade
             self.notify_trade(trade, trade_subscribers)
-
+            
             if lowest_ask.is_exhausted():
                 self.asks.remove(ask_key)
-
+        
         return trade
-
+    
     # O(log(n))
     def _try_sell(self, ask: Order) -> Trade or None:
         """
@@ -411,24 +408,24 @@ class OrderBook:
         """
         if self.bids.is_empty():
             return None
-
+        
         bid_key, highest_bid = self.bids.max_item()
         trade = None
         if highest_bid >= ask:  # someone bid high enough and a trade will be made
             trade = Trade(highest_bid, ask)
             self.logger.log_trade(trade)
-
+            
             # get all who subscribed to either side
             trade_subscribers = self._subscribers_of_orders(highest_bid, ask)
-
+            
             # notify them about the trade
             self.notify_trade(trade, trade_subscribers)
-
+            
             if highest_bid.is_exhausted():
                 self.bids.remove(bid_key)
-
+        
         return trade
-
+    
     def _subscribers_of_orders(self, *orders):
         """Returns a list of Subscribers that have subscribed to any of the passed orders"""
         return [sub for sub in self.subscribers.values()
--- tests/test_orderbook.py
+++ tests/test_orderbook.py
@@ -18,7 +18,7 @@ if m:
 
 class TestOrderBook(unittest.TestCase):
     REMOVE_LOG_FILES = False  # set True to delete all log files created by the tests after done running
-
+    
     @classmethod
     def tearDownClass(cls):
         """After all tests are done, deletes all logs created by the tests if REMOVE_LOG_FILES == True"""
@@ -31,15 +31,15 @@ class TestOrderBook(unittest.TestCase):
                 if test_logfile:
                     # file is a test log file created by one of the test methods
                     os.remove(os.path.join(TESTS_FOLDER_NAME, test_logfile.group(0)))
-
+    
     @staticmethod
     def create_bid(price, size):
         return Order(Order.BID, price, size)
-
+    
     @staticmethod
     def create_ask(price, size):
         return Order(Order.ASK, price, size)
-
+    
     def create_mock_orderbook(self, logfile_full_path):
         """
         Creates a mock orderbook which contains 4 bids (price, size):
@@ -54,9 +54,9 @@ class TestOrderBook(unittest.TestCase):
         order_book.add_order(self.create_bid(70, 15), random_str())
         order_book.add_order(self.create_bid(99, 7), random_str())
         order_book.add_order(self.create_bid(101, 9), random_str())
-
+        
         return order_book
-
+    
     def tearDown(self):
         """Since:
         1) OrderBook methods are likely to write to a log file, and
@@ -66,16 +66,16 @@ class TestOrderBook(unittest.TestCase):
         """
         super().tearDown()
         current_logger = logging.getLogger()
-
+        
         if not current_logger.handlers:
             # "test_bad_order_side()" doesn't instanciate an OrderBook
             # so no logging is done == no handlers to close and remove
             return
-
+        
         handler = current_logger.handlers[0]
         handler.close()
         current_logger.removeHandler(handler)
-
+    
     def test_add_ask_yes_bid_exhaust(self):
         order_book = self.create_mock_orderbook(f'{TESTS_FOLDER_NAME}/{self._testMethodName}.log')
         _, min_bid_before = order_book.bids.min_item()
@@ -86,27 +86,27 @@ class TestOrderBook(unittest.TestCase):
         self.assertEqual(min_bid_before.price, 70)
         self.assertEqual(max_bid_before.size, 9)
         self.assertEqual(max_bid_before.price, 101)
-
+        
         order_book.add_order(self.create_ask(99, 26), random_str())
-
+        
         self.assertEqual(order_book.bids.count, 1)
         self.assertEqual(order_book.asks.count, 0)
-
+        
         _, min_bid_after = order_book.bids.min_item()
         self.assertEqual(min_bid_after.size, 15)
         self.assertEqual(min_bid_after.price, 70)
         self.assertEqual(min_bid_after, min_bid_before)
-
+        
         self.assertIsNone(max_bid_before.size)
         self.assertTrue(max_bid_before.is_exhausted())
-
+        
         _, max_bid_after = order_book.bids.max_item()
         self.assertEqual(max_bid_after, min_bid_after)
         self.assertNotEqual(max_bid_after, max_bid_before)
-
+    
     def test_add_ask_no_bid_exhaust(self):
         order_book = self.create_mock_orderbook(f'{TESTS_FOLDER_NAME}/{self._testMethodName}.log')
-
+        
         min_bid_key, min_bid = order_book.bids.min_item()
         max_bid_key, max_bid = order_book.bids.max_item()
         self.assertEqual(order_book.bids.count, 4)
@@ -115,62 +115,62 @@ class TestOrderBook(unittest.TestCase):
         self.assertEqual(min_bid.price, 70)
         self.assertEqual(max_bid.size, 9)
         self.assertEqual(max_bid.price, 101)
-
+        
         order_book.add_order(self.create_ask(99, 8), random_str())
-
+        
         self.assertEqual(order_book.bids.count, 4)
         self.assertTrue(order_book.asks.is_empty())
-
+        
         self.assertEqual(min_bid.size, 15)
         self.assertEqual(min_bid.price, 70)
-
+        
         self.assertEqual(max_bid.size, 1)
         self.assertEqual(max_bid.price, 101)
-
+    
     def test_show_orderbook(self):
         order_book = self.create_mock_orderbook(f'{TESTS_FOLDER_NAME}/{self._testMethodName}.log')
         output = order_book.show_orderbook()
         lines = output.splitlines()
-
-        regex = ['^$',
-                 '^Bids:$',
-                 '^\(0\) timestamp: \d{10}\.\d{5,7}, side: b, price: 70, size: 15, id: \d{13}, sender_id: "[a-zA-Z0-9]{8}"',
-                 '^\(1\) timestamp: \d{10}\.\d{5,7}, side: b, price: 99, size: 7, id: \d{13}, sender_id: "[a-zA-Z0-9]{8}"',
-                 '^\(2\) timestamp: \d{10}\.\d{5,7}, side: b, price: 100, size: 10, id: \d{13}, sender_id: "[a-zA-Z0-9]{8}"',
-                 '^\(3\) timestamp: \d{10}\.\d{5,7}, side: b, price: 101, size: 9, id: \d{13}, sender_id: "[a-zA-Z0-9]{8}"',
-                 '^Asks:$',
-                 '-- No asks --',
-                 ]
-
+        
+        regex = list(map(re.compile, ('^$',
+                                      '^Bids:$',
+                                      r'^\(0\) timestamp: \d{10}\.\d{5,7}, side: b, price: 70, size: 15, id: \d{15}, sender_id: "[a-zA-Z0-9]{8}"',
+                                      r'^\(1\) timestamp: \d{10}\.\d{5,7}, side: b, price: 99, size: 7, id: \d{15}, sender_id: "[a-zA-Z0-9]{8}"',
+                                      r'^\(2\) timestamp: \d{10}\.\d{5,7}, side: b, price: 100, size: 10, id: \d{15}, sender_id: "[a-zA-Z0-9]{8}"',
+                                      r'^\(3\) timestamp: \d{10}\.\d{5,7}, side: b, price: 101, size: 9, id: \d{15}, sender_id: "[a-zA-Z0-9]{8}"',
+                                      '^Asks:$',
+                                      '-- No asks --',
+                                      )
+                         ))
         for i, line in enumerate(lines):
             self.assertRegex(line, regex[i])
-
+        
         # remove all bids
         for bid in list(order_book.bids.values()):
             order_book.remove_order(bid.id, bid.sender_id)
-
+        
         regex = ['^$',
                  '^Bids:$',
                  '-- No bids --',
                  '^Asks:$',
                  '-- No asks --',
                  ]
-
+        
         for i, line in enumerate(order_book.show_orderbook().splitlines()):
             self.assertRegex(line, regex[i])
-
+    
     def test_show_trades(self):
         import time
         order_book = self.create_mock_orderbook(f'{TESTS_FOLDER_NAME}/{self._testMethodName}.log')
         for _ in range(26):
             order_book.add_order(self.create_ask(99, 1), random_str())
             time.sleep(0.01)
-
+        
         trades = order_book.show_trades()
         trades_are_timestamp_sorted = all(
-            [trade.timestamp < trades[i + 1].timestamp for i, trade in enumerate(trades[:-1])])
+                [trade.timestamp < trades[i + 1].timestamp for i, trade in enumerate(trades[:-1])])
         self.assertTrue(trades_are_timestamp_sorted)
-
+    
     def test_show_top(self):
         new_log_file = f'{TESTS_FOLDER_NAME}/{self._testMethodName}.log'
         order_book = OrderBook(new_log_file)
@@ -178,26 +178,26 @@ class TestOrderBook(unittest.TestCase):
         order_book.add_order(self.create_ask(100, 29), random_str())
         order_book.add_order(self.create_bid(10, 3), random_str())
         order_book.add_order(self.create_bid(10, 4), random_str())
-
+        
         highest_bid, lowest_ask = order_book.show_top()
-
+        
         # Test overridden Order comparison functions (by _key())
         self.assertTrue((highest_bid.price, highest_bid.size), (10, 4))
         self.assertTrue((lowest_ask.price, lowest_ask.size), (100, 29))
-
+        
         order_book.add_order(self.create_ask(99, 28), random_str())
         order_book.add_order(self.create_bid(11, 5), random_str())
-
+        
         highest_bid, lowest_ask = order_book.show_top()
         self.assertTrue((highest_bid.price, highest_bid.size), (11, 5))
         self.assertTrue((lowest_ask.price, lowest_ask.size), (99, 28))
-
+    
     def test_order_size_setter(self):
         order = Order("a", 100, 5)
         order.size = -5
         self.assertIsNone(order.size)
         self.assertTrue(order.is_exhausted())
-
+    
     def test_bad_order_side(self):
         bad_side = "not a nor b"
         err_regex = '\n'.join([
@@ -206,6 +206,7 @@ class TestOrderBook(unittest.TestCase):
             ])
         with self.assertRaisesRegex(ValueError, err_regex):
             Order(bad_side, 100, 5)
+    
     def test_remove_order(self):
         order_book = self.create_mock_orderbook(f'{TESTS_FOLDER_NAME}/{self._testMethodName}.log')
         ask = self.create_ask(100, 30)
@@ -214,23 +215,23 @@ class TestOrderBook(unittest.TestCase):
         self.assertEqual(order_book.bids.count, 3)
         order_book.remove_order(ask.id, ask.sender_id)
         self.assertEqual(order_book.asks.count, 0)
-
+        
         # Copy the values, so removing their references won't interfere with testing
         bids_copy = list(order_book.bids.values())
         bids_ids = [bid.id for bid in bids_copy]
-
+        
         for bid_id in bids_ids:
             # Should not remove orders with no matching sender id.
             order_book.remove_order(bid_id, "BAD SENDER ID")
-
+        
         self.assertEqual(order_book.bids.count, 3)  # no bids_copy were removed
-
+        
         for bid in bids_copy:
             # Should remove
             order_book.remove_order(bid.id, bid.sender_id)
-
+        
         self.assertEqual(order_book.bids.count, 0)
-
+    
     def test_logger_no_remove(self):
         new_log_file = f'{TESTS_FOLDER_NAME}/{self._testMethodName}.log'
         order_book = OrderBook(new_log_file)
@@ -240,15 +241,15 @@ class TestOrderBook(unittest.TestCase):
             lines = f.readlines()
             # the data in the now created log file is expected to match the following regex expressions
             regex = [
-                'BID \| timestamp: \d{10}\.\d{5,7}, side: b, price: 100, size: 5, id: \d{13}, sender_id: "[a-zA-Z0-9]{8}"',
-                'ASK \| timestamp: \d{10}\.\d{5,7}, side: a, price: 90, size: 7, id: \d{13}, sender_id: "[a-zA-Z0-9]{8}"',
-                'TRADE \| timestamp: \d{10}\.\d{5,7}, price: 100, size: 5, buyer_id: "[a-zA-Z0-9]{8}", seller_id: "[a-zA-Z0-9]{8}"',
-                '\t--> ASK id: \d{13} now has size: 2',
-                '\t--> BID id: \d{13} now has been exhausted',
+                r'BID \| timestamp: \d{10}\.\d{5,7}, side: b, price: 100, size: 5, id: \d{15}, sender_id: "[a-zA-Z0-9]{8}"',
+                r'ASK \| timestamp: \d{10}\.\d{5,7}, side: a, price: 90, size: 7, id: \d{15}, sender_id: "[a-zA-Z0-9]{8}"',
+                r'TRADE \| timestamp: \d{10}\.\d{5,7}, price: 100, size: 5, buyer_id: "[a-zA-Z0-9]{8}", seller_id: "[a-zA-Z0-9]{8}"',
+                r'\t--> ASK id: \d{15} now has size: 2',
+                r'\t--> BID id: \d{15} now has been exhausted',
                 ]
             for i, line in enumerate(lines):
                 self.assertRegex(line, regex[i])
-
+    
     def test_logger_yes_remove(self):
         new_log_file = f'{TESTS_FOLDER_NAME}/{self._testMethodName}.log'
         order_book = OrderBook(new_log_file)
@@ -258,21 +259,22 @@ class TestOrderBook(unittest.TestCase):
         order_book.add_order(ask, random_str())
         with self.assertRaises(KeyError) as e:
             order_book.remove_order(bid.id, bid.sender_id)
-            err_regex = '^Tried to remove order but no such order exists\.\\nOrder id: \d{13}\. Order key: \(100, 5\)$'
+            err_regex = r'^Tried to remove order but no such order exists\.\\nOrder id: \d{15}\. Order key: \(100, 5\)$'
             self.assertRegex(e.args[0], err_regex)
-
+        
         order_book.remove_order(ask.id, ask.sender_id)
         with open(new_log_file, 'r') as f:
             lines = f.readlines()
             # the data in the now created log file is expected to match the following regex expressions
-            regex = [
-                'BID \| timestamp: \d{10}\.\d{5,7}, side: b, price: 100, size: 5, id: \d{13}, sender_id: "[a-zA-Z0-9]{8}"',
-                'ASK \| timestamp: \d{10}\.\d{5,7}, side: a, price: 90, size: 7, id: \d{13}, sender_id: "[a-zA-Z0-9]{8}"',
-                'TRADE \| timestamp: \d{10}\.\d{5,7}, price: 100, size: 5, buyer_id: "[a-zA-Z0-9]{8}", seller_id: "[a-zA-Z0-9]{8}"',
-                '\t--> ASK id: \d{13} now has size: 2',
-                '\t--> BID id: \d{13} now has been exhausted',
-                'ASK (rm) | timestamp: \d{10}\.\d{5,7}, side: a, price: 90, size: 2, id: \d{13}, sender_id: "[a-zA-Z0-9]{8}"'
+            regex = list(map(re.compile, [
+                r'BID \| timestamp: \d{10}\.\d{5,7}, side: b, price: 100, size: 5, id: \d{15}, sender_id: "[a-zA-Z0-9]{8}"',
+                r'ASK \| timestamp: \d{10}\.\d{5,7}, side: a, price: 90, size: 7, id: \d{15}, sender_id: "[a-zA-Z0-9]{8}"',
+                r'TRADE \| timestamp: \d{10}\.\d{5,7}, price: 100, size: 5, buyer_id: "[a-zA-Z0-9]{8}", seller_id: "[a-zA-Z0-9]{8}"',
+                r'\t--> ASK id: \d{15} now has size: 2',
+                r'\t--> BID id: \d{15} now has been exhausted',
+                r'ASK (rm) | timestamp: \d{10}\.\d{5,7}, side: a, price: 90, size: 2, id: \d{15}, sender_id: "[a-zA-Z0-9]{8}"'
                 ]
+                             ))
             for i, line in enumerate(lines):
                 self.assertRegex(line, regex[i])
 
